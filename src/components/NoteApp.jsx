ec1ed8f201d22f4d177131aaed983fd274370f53
Tari Puspita Sari <taripuspitasari12@gmail.com>
2023-12-03 12:03:52 +0700

    Adding input form

--- src/components/NoteApp.jsx
+++ src/components/NoteApp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NoteList from "./NoteList";
 import {getInitialData} from "../utils/index";
+import NoteInput from "./NoteInput";
 
 class NoteApp extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class NoteApp extends React.Component {
     };
 
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
   }
 
   onDeleteHandler(id) {
@@ -17,10 +19,27 @@ class NoteApp extends React.Component {
     this.setState({notes});
   }
 
+  onAddNoteHandler({title, body}) {
+    this.setState(prevState => {
+      return {
+        notes: [
+          ...prevState.notes,
+          {
+            id: +new Date(),
+            title,
+            body,
+          },
+        ],
+      };
+    });
+  }
+
   render() {
     return (
       <div className="note-app__body">
         <h1 className="note-app__header">Notes</h1>
+        <h2>Buat Catatan</h2>
+        <NoteInput addNote={this.onAddNoteHandler} />
         <h2>Catatan Aktif</h2>
         <NoteList notes={this.state.notes} onDelete={this.onDeleteHandler} />
       </div>
--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {showFormattedDate} from "../utils";
+// import {showFormattedDate} from "../utils";
 
 class NoteInput extends React.Component {
   constructor(props) {
@@ -8,8 +8,53 @@ class NoteInput extends React.Component {
     this.state = {
       title: "",
       body: "",
-      createdAt: showFormattedDate(),
+      // createdAt: showFormattedDate(),
     };
+
+    this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
+    this.onBodyChangeEventHandler = this.onBodyChangeEventHandler.bind(this);
+    this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+  }
+
+  onTitleChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        title: event.target.value,
+      };
+    });
+  }
+
+  onBodyChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        body: event.target.value,
+      };
+    });
+  }
+
+  onSubmitEventHandler(event) {
+    event.preventDefault();
+    this.props.addNote(this.state);
+  }
+
+  render() {
+    return (
+      <form className="note-input" onSubmit={this.onSubmitEventHandler}>
+        <input
+          type="text"
+          placeholder="Ini adalah judul..."
+          value={this.state.title}
+          onChange={this.onTitleChangeEventHandler}
+        />
+        <input
+          type="text"
+          placeholder="Tuliskan catatanmu disini..."
+          value={this.state.body}
+          onChange={this.onBodyChangeEventHandler}
+        />
+        <button type="submit">Buat</button>
+      </form>
+    );
   }
 }
 
